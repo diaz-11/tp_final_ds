cad5e0fcfc3d6b40e2d48babf4e452e9f8de6b41
diaz-11 <angiediazvilca12@gmail.com>
2023-11-06 16:28:01 -0300

    track.index

---
+++ chinook/album.py
@@ -0,0 +1,21 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.db import get_db
+
+bp = Blueprint('album', __name__,url_prefix='/album')
+
+@bp.route('/')
+def index():
+    db = get_db()
+    albums = db.execute(
+        """SELECT tracks.Name , albums.Title
+        FROM tracks 
+        JOIN albums 
+        ON albums.AlbumId = tracks.AlbumId
+        ORDER BY albums.Title DESC"""
+    ).fetchall()
+    return render_template('albums/index.html', albums=albums)
+
---
+++ chinook/artists.py
@@ -0,0 +1,22 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.db import get_db
+
+bp = Blueprint('artist', __name__,url_prefix='/artist')
+
+@bp.route('/')
+def index():
+    db = get_db()
+    artists = db.execute(
+        """SELECT tracks.Name , albums.Title, artists.Name
+        FROM tracks 
+        JOIN albums 
+        ON albums.AlbumId = tracks.AlbumId
+        JOIN artists 
+        ON albums.ArtistId = artists.ArtistId
+        ORDER BY artists.Name ASC"""
+    ).fetchall()
+    return render_template('artists/index.html', artists=artists)
